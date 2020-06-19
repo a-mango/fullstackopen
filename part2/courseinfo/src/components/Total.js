7dceb1e3c7b34795e6a2776b2c987e866ce9c3f5
Aubry Mangold <aubry.mangold@outlook.com>
2020-06-20 00:32:20 +0200

    Complete 2.2 Course contents step 8

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,12 +1,8 @@
 import React from "react";
 
 const Total = ({ parts }) => {
-  let sum = 0;
-  for(const part of parts) {
-    sum += part.exercises
-  }
-
-  return <p><strong>Total of {sum} exercises</strong></p>;
+  const total = parts.reduce((s, p) => s + p.exercises, 0)
+  return <p><strong>Total of {total} exercises</strong></p>;
 };
 
 export default Total;
