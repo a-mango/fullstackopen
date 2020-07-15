b1496501008b86b5eebbcbe31b0bcd894028ca86
Aubry Mangold <aubry.mangold@outlook.com>
2020-07-15 16:01:22 +0200

    Remove styling from countries app

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./mini.min.css";
 import Filter from "./components/Filter";
 import Countries from "./components/Countries";
 
