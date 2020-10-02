3f1ba78c14526ba2d86031335e1eb48eb3994d7e
Aubry Mangold <aubry.mangold@outlook.com>
2020-10-02 12:49:29 +0200

    Add favorite blog unit test

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -7,7 +7,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  let blogWithMostLikes = blogs[0]
+
+  // Iterate through the blogs array to find the maximum blog.likes value
+  blogs.forEach((blog) => {
+    blog.likes > blogWithMostLikes.likes ? (blogWithMostLikes = blog) : false
+  })
+
+  return blogWithMostLikes
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
