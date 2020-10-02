e4fee850dcea56f09f95f6ee2c04f9c88d49cc4a
Aubry Mangold <aubry.mangold@outlook.com>
2020-10-02 12:22:58 +0200

    Add total likes unit test

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+}
