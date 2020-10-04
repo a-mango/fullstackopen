1b9108ab774db0c6b6ef9748596610d1dab21147
Aubry Mangold <aubry.mangold@outlook.com>
2020-10-04 16:37:36 +0200

    Modify directory name

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -9,12 +9,24 @@ const Blog = require('../models/blog')
  * Operations to run before each test session
  */
 beforeEach(async () => {
+  // Clear the database
   await Blog.deleteMany({})
 
+  // Create an array of Blog models using initialBlogs
   const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  // Create an array of promises
   const promiseArray = blogObjects.map(blog => blog.save())
+  // Execute all promises
+  await Promise.all(promiseArray)
+})
 
-  Promise.all(promiseArray)
+/**
+ * Test if all the items saved to the database are returned
+ */
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response).toHaveLength(helper.initialBlogs.length)
 })
 
 /**
