bdf962833731eeba8c9efea4bfec61a8a55dd859
Aubry Mangold <aubry.mangold@outlook.com>
2020-10-13 17:53:57 +0200

    Modify user model
    Add unique and required fields to userSchema

--- part4/models/user.js
+++ part4/models/user.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   name: String,
-  passwordHash: String,
+  passwordHash: {
+    type: String,
+    required: true
+  },
   blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
@@ -11,6 +19,8 @@ const userSchema = new mongoose.Schema({
     },
   ],
 })
+
+userSchema.plugin(uniqueValidator)
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
