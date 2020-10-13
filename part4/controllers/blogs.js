b9524b9f617e9a21f4644bc8b45281706d6cb35e
Aubry Mangold <aubry.mangold@outlook.com>
2020-10-13 21:30:47 +0200

    Modify blog post to require authentication

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 /**
  * Blog api get route
@@ -15,12 +24,17 @@ blogsRouter.get('/', async (request, response) => {
  */
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findOne({})
-
   if (!body.title || !body.url) {
     return response.status(400).json({ error: 'missing title or url' })
   }
+
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     user: user.id,
