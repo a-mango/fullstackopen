6a96e42822b98e93409f636baa03b7958beea6fc
Aubry Mangold <aubry.mangold@outlook.com>
2020-10-14 20:17:20 +0200

    Expand app to allow blog creation

--- part5/src/App.js
+++ part5/src/App.js
@@ -8,6 +8,9 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   /**
    * Fetch all blogs when app is rendered
@@ -26,17 +29,17 @@ const App = () => {
       const user = JSON.parse(loggedUserJson)
       setUser(user)
       blogService.setToken(user.token)
+      console.log(`Authenticated as ${user.username}`)
     }
   }, [])
 
   /**
    * Handle login of the user by making a call to the
-   * login api and saving the returned user to state 
+   * login api and saving the returned user to state
    * and localStorage
    */
   const handleLogin = async event => {
     event.preventDefault()
-    console.log(username, password)
     try {
       const user = await loginService.login({
         username,
@@ -55,7 +58,7 @@ const App = () => {
       setPassword('')
 
       console.log(`Authenticated as ${user.username}`)
-    } catch {
+    } catch (exception) {
       console.error('Authentication failed: wrong credentials')
     }
   }
@@ -69,50 +72,129 @@ const App = () => {
     window.localStorage.removeItem('loggedBlogAppUser')
     // Reset state
     setUser(null)
-    
+
     console.log(`User logged out`)
   }
 
+  /**
+   * Handle addition of a blog
+   */
+  const addBlog = async event => {
+    event.preventDefault()
+
+    // Create an object with data from form
+    const blogObject = {
+      title,
+      author,
+      url,
+    }
+
+    try {
+      // Post the data to the server
+      const returnedBlog = await blogService.create(blogObject)
+
+      // Update state
+      setBlogs(blogs.concat(returnedBlog))
+
+      // Reset form state
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch (exception) {
+      console.error('Blog creation failed')
+    }
+  }
+  /**
+   * Login form component
+   */
   const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        <label>Username</label>
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+    <div>
+      <h2>Log in to the application</h2>
+
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            value={username}
+            name="username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            value={password}
+            name="password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+
+  /**
+   * Blog form component
+   */
+  const blogForm = () => (
+    <div>
+      <h2>Add a new blog</h2>
+
+      <form onSubmit={addBlog}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            value={title}
+            name="title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            value={author}
+            name="author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="url">Url:</label>
+          <input
+            type="text"
+            value={url}
+            name="url"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">Add blog</button>
+      </form>
+    </div>
+  )
+
+  const logoutForm = () => (
+    <div>
+      <p>Logged in as {user.username}</p>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
   )
 
   // Show the login form if user is not authenticated
   if (user === null) {
-    return (
-      <div>
-        <h1>Log in to the application</h1>
-        {loginForm()}
-      </div>
-    )
+    return loginForm()
   }
 
   // Show all blogs when user is logged in
   return (
     <div>
       <h1>Blogs</h1>
-      <p>Logged in as {user.username}</p>
-      <button onClick={handleLogout}>Logout</button>
+      {logoutForm()}
+      {blogForm()}
+
+      <h2>Saved blogs</h2>
       {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
       ))}
--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -5,10 +5,18 @@ let token = null
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
-  console.log(response)
+  return response.data
+}
+
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const response = await axios.post(baseUrl, newObject, config)
   return response.data
 }
 
 const setToken = newToken => (token = `bearer ${newToken}`)
 
-export default { getAll, setToken }
+export default { getAll, create, setToken }
