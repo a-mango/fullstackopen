151754576b8ba0041e9af9bb33f9fbdd038bc69d
Aubry Mangold <aubry.mangold@outlook.com>
2020-10-14 18:48:51 +0200

    Implement basic login functionality

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,22 +1,83 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then(blogs => setBlogs(blogs))
   }, [])
 
+  const handleLogin = async event => {
+    event.preventDefault()
+    console.log(username, password)
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      console.log(`Authenticated as ${user.username}`)
+    } catch {
+      console.error('Authentication failed: wrong credentials')
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>Username</label>
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  )
+
+  /**
+   * Show the login form if user is not authenticated
+   */
+  if (user === null) {
+    return (
+      <div>
+        <h1>Log in to the application</h1>
+        {loginForm()}
+      </div>
+    )
+  }
+
+  /**
+   * Show all blogs when user is logged in
+   */
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
+      <h1>Blogs</h1>
+      <p>Logged in as {user.username}</p>
+      {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
   )
 }
--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+let token = null
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  console.log(response)
+  return response.data
 }
 
-export default { getAll }
+const setToken = newToken => (token = `bearer ${newToken}`)
+
+export default { getAll, setToken }
