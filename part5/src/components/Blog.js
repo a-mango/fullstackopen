6b8908ee5c4b118cf94576e175ae4012e13809b8
Aubry Mangold <aubry.mangold@outlook.com>
2020-10-15 15:15:50 +0200

    Expand Blog component to add detailed view

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,56 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const { title, author, url, likes } = blog
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => setVisible(!visible)
+
+  const blogStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    border: '1px solid #242424',
+    padding: '0.2em',
+    marginBottom: '0.4em',
+  }
+
+  return (
+    <div style={blogStyle}>
+      {visible ? (
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <td>Title</td>
+                <td>{title}</td>
+              </tr>
+              <tr>
+                <td>Author</td>
+                <td>{author}</td>
+              </tr>
+              <tr>
+                <td>URL</td>
+                <td>
+                  <a href={url}>{url}</a>
+                </td>
+              </tr>
+              <tr>
+                <td>Likes</td>
+                <td>{likes}</td>
+              </tr>
+            </tbody>
+          </table>
+          <button>Like</button>
+        </div>
+      ) : (
+        <div>
+          <p>
+            {title} - {author}
+          </p>
+        </div>
+      )}
+      <button onClick={toggleVisibility}>{visible ? 'Hide' : 'View'}</button>
+    </div>
+  )
+}
 
 export default Blog
