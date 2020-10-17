80e49635c338b68e330ab71de80fa6f699e0d83b
Aubry Mangold <aubry.mangold@outlook.com>
2020-10-17 17:52:17 +0200

    Remove unused import

--- part5/src/components/BlogForm.test.js
+++ part5/src/components/BlogForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent, getByLabelText } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import BlogForm from './BlogForm'
 
