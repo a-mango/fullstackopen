57526957cf6f6e466c6240307561c4f8a0e8f49c
Aubry Mangold <aubry.mangold@outlook.com>
2020-10-23 19:54:09 +0200

    Add ids to BlogForm elements

--- part5/src/components/BlogForm.js
+++ part5/src/components/BlogForm.js
@@ -25,12 +25,12 @@ const BlogForm = ({ createBlog }) => {
     <div className="BlogForm">
       <h2>Add a new blog</h2>
 
-      <form onSubmit={addBlog}>
+      <form id="blog-form" onSubmit={addBlog}>
         <fieldset>
           <div>
             <label htmlFor="title">Title:</label>
             <input
-              id="title"
+              id="blog-title"
               type="text"
               value={title}
               name="title"
@@ -40,7 +40,7 @@ const BlogForm = ({ createBlog }) => {
           <div>
             <label htmlFor="author">Author:</label>
             <input
-              id="author"
+              id="blog-author"
               type="text"
               value={author}
               name="author"
@@ -50,14 +50,16 @@ const BlogForm = ({ createBlog }) => {
           <div>
             <label htmlFor="url">Url:</label>
             <input
-              id="url"
+              id="blog-url"
               type="text"
               value={url}
               name="url"
               onChange={({ target }) => setUrl(target.value)}
             />
           </div>
-          <button type="submit">Add blog</button>
+          <button id="blog-submit" type="submit">
+            Add blog
+          </button>
         </fieldset>
       </form>
     </div>
