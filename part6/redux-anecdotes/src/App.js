07b3a29578d1b00a3ef321e81b41bdacb2f9806c
Aubry Mangold <aubry.mangold@outlook.com>
2020-10-29 11:28:00 +0100

    Implement fetching anecdotes from the database

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import anecdoteService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => {
+      dispatch(initializeAnecdotes(anecdotes))
+    })
+  }, [dispatch])
+  
   return (
     <div>
       <h2>Anecdotes</h2>
