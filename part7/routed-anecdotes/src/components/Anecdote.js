8e94dc16cac83e2372359e813705f8a762e1d910
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-13 15:48:56 +0100

    Extract components

---
+++ part7/routed-anecdotes/src/components/Anecdote.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Anecdote = ({ anecdote }) => (
+  <div>
+    <h2>{anecdote.content}</h2>
+    <p>has {anecdote.votes} votes</p>
+  </div>
+)
+
+export default Anecdote
