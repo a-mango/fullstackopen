745c136a1bf68e1bf9e933953644c5f84a530f92
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-13 17:54:46 +0100

    Refactor useCountry hook to fetch data

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -1,24 +1,39 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const useField = (type) => {
+const useField = type => {
   const [value, setValue] = useState('')
 
-  const onChange = (event) => {
+  const onChange = event => {
     setValue(event.target.value)
   }
 
   return {
     type,
     value,
-    onChange
+    onChange,
   }
 }
 
-const useCountry = (name) => {
+const useCountry = name => {
   const [country, setCountry] = useState(null)
 
-  useEffect()
+  useEffect(() => {
+    axios
+      .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+      .then(response => {
+        setCountry({
+          found: true,
+          data: response.data[0],
+        })
+      })
+      .catch(e => {
+        setCountry({
+          found: false,
+          data: null,
+        })
+      })
+  }, [name])
 
   return country
 }
@@ -29,19 +44,19 @@ const Country = ({ country }) => {
   }
 
   if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div>not found...</div>
   }
 
   return (
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img
+        src={country.data.flag}
+        height="100"
+        alt={`flag of ${country.data.name}`}
+      />
     </div>
   )
 }
@@ -51,7 +66,7 @@ const App = () => {
   const [name, setName] = useState('')
   const country = useCountry(name)
 
-  const fetch = (e) => {
+  const fetch = e => {
     e.preventDefault()
     setName(nameInput.value)
   }
