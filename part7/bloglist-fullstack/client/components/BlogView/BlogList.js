2110fe00ae3a606839e23ac166a01618ed2cef50
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-16 14:05:53 +0100

    Refactor BlogList to use redux

--- part7/bloglist-fullstack/client/components/BlogView/BlogList.js
+++ part7/bloglist-fullstack/client/components/BlogView/BlogList.js
@@ -1,31 +1,30 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import { useSelector } from 'react-redux'
 import Blog from './Blog'
 
-const BlogList = ({ blogs, user, updateBlog, removeBlog }) => {
+const BlogList = () => {
+  const blogs = useSelector(state => state.blogs)
+
   const style = {
     width: '35vw',
   }
 
   return (
-    <div className='BlogList'>
+    <div className="BlogList">
       <h2>Saved blogs</h2>
       <div style={style}>
-        {blogs && blogs
-          .sort((a, b) => b.likes - a.likes)
-          .map(blog => (
-            <Blog key={blog.id} blog={blog} updateBlog={updateBlog} removeBlog={removeBlog} user={user} />
-          ))}
+        {blogs &&
+          blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map(blog => (
+              <Blog
+                key={blog.id}
+                blog={blog}
+              />
+            ))}
       </div>
     </div>
   )
-}
-
-BlogList.propTypes = {
-  blogs: PropTypes.array.isRequired,
-  user: PropTypes.shape().isRequired,
-  updateBlog: PropTypes.func.isRequired,
-  removeBlog: PropTypes.func.isRequired
 }
 
 export default BlogList
