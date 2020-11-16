f4c1ca944c8b6f81a917a560d673f2034270b920
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-16 12:48:26 +0100

    Sort components

---
+++ part7/bloglist-fullstack/client/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Created for{' '}
+        <a href="https://fullstackopen.com/en/">Fullstackopen 2020</a> by{' '}
+        <a href="https://github.com/a-mango">a-mango</a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ part7/bloglist-fullstack/client/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <header>
+      <h1>Bloglist</h1>
+      <nav>
+        <p>Home</p>
+        <p>Blogs</p>
+        <p>About</p>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav
