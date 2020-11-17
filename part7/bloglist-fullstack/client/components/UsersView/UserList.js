bc6821bdc517cb6b0a7e136323da229a43d45874
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-17 15:02:21 +0100

    Refactor UserList component

--- part7/bloglist-fullstack/client/components/UsersView/UserList.js
+++ part7/bloglist-fullstack/client/components/UsersView/UserList.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import useResource from 'Hooks/use-resource'
-import {apiUrl} from 'Utilities/common'
+import { apiUrl } from 'Utilities/common'
 
 const UserList = () => {
   const [users, userService] = useResource(`${apiUrl}/users`)
-
+  console.log(users)
   return (
     <div>
-      {
-        users.map(user => (<p>{user.username}</p>))
-      }
+      <table>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Blogs created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr>
+              <td>{user.username}</td>
+              <td>{user.blogs.length}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
