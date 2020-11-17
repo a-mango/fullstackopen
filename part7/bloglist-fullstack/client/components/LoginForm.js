87757a44553b6e500e5433f86bdf66add64c8fcd
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-17 13:56:58 +0100

    Refactor LoginForm to use redux

--- part7/bloglist-fullstack/client/components/LoginForm.js
+++ part7/bloglist-fullstack/client/components/LoginForm.js
@@ -1,17 +1,54 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setUser } from 'Utilities/reducers/userReducer'
+import { setNotification } from 'Utilities/reducers/notificationReducer'
+import loginService from 'Utilities/services/login'
 
-const LoginForm = ({ loginUser }) => {
+const LoginForm = () => {
+  const dispatch = useDispatch()
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleLogin = event => {
+  useEffect(() => {
+    const loggedUserJson = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJson) {
+      const user = JSON.parse(loggedUserJson)
+      dispatch(setUser(user))
+      dispatch(
+        setNotification({
+          type: 'info',
+          message: `Automatically logged in as ${user.username}`,
+        })
+      )
+    }
+  }, [dispatch])
+
+  const handleLogin = async event => {
     event.preventDefault()
 
-    loginUser({ username, password })
-    setUsername('')
-    setPassword('')
+    try {
+      const user = await loginService.login({ username, password })
+
+      dispatch(setUser(user))
+      setUsername('')
+      setPassword('')
+
+      dispatch(
+        setNotification({
+          type: 'success',
+          message: `Successfully logged in as ${username}`,
+        })
+      )
+    } catch (exception) {
+      dispatch(
+        setNotification({
+          type: 'error',
+          message: `An error has occured while attempting to log in`,
+        })
+      )
+    }
   }
+
   return (
     <div>
       <h2>Log in to the application</h2>
@@ -43,10 +80,6 @@ const LoginForm = ({ loginUser }) => {
       </form>
     </div>
   )
-}
-
-LoginForm.propTypes = {
-  loginUser: PropTypes.func.isRequired,
 }
 
 export default LoginForm
