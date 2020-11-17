424ca98115654d838e64bbcd4acdf9c0015d8629
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-17 14:30:08 +0100

    Add useResource hook

---
+++ part7/bloglist-fullstack/client/hooks/use-resource.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+
+const useResource = baseUrl => {
+  const [resources, setResources] = useState([])
+  const [headers, setHeaders] = useState(null)
+
+  useEffect(() => {
+    axios.get(baseUrl, headers).then(response => {
+      setResources(response.data)
+    })
+  }, [baseUrl])
+
+  const setToken = token => {
+    setHeaders({ Authorization: `bearer ${token}` })
+  }
+
+  const create = async resource => {
+    const response = await axios.post(baseUrl, resource, headers)
+    setResources(resources.concat(response.data))
+    return response.data
+  }
+
+  const get = async id => {
+    const response = await axios.get(`${baseUrl}/${id}`, resource, headers)
+    return response.data
+  }
+
+  const getAll = async () => {}
+
+  const remove = async id => {
+    await axios.delete(`${baseUrl}/${id}`, headers)
+    setResources(
+      resources.filter(resource => (resource.id !== id ? resource : null))
+    )
+  }
+
+  const update = async (id, resource) => {
+    const response = await axios.put(`${baseUrl}/${id}`, resource, headers)
+    setResources(
+      resources.filter(resource =>
+        resource.id !== id ? resource : response.data
+      )
+    )
+    return response.data
+  }
+
+  const service = {
+    create,
+    get,
+    getAll,
+    remove,
+    update,
+  }
+
+  return [resources, service, setToken]
+}
+
+export default useResource
