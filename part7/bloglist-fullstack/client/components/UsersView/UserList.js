b58d69b224e6e9be4f94e3aa76257a37fc92f4c1
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-17 18:33:50 +0100

    Add contitional rendering

--- part7/bloglist-fullstack/client/components/UsersView/UserList.js
+++ part7/bloglist-fullstack/client/components/UsersView/UserList.js
@@ -12,14 +12,16 @@ const UserList = ({ users }) => (
         </tr>
       </thead>
       <tbody>
-        {users.map(user => (
-          <tr key={user.username}>
-            <td>
-              <Link to={`users/${user.id}`}>{user.username}</Link>
-            </td>
-            <td>{user.blogs.length}</td>
-          </tr>
-        ))}
+        {users &&
+          users
+            .map(user => (
+              <tr key={user.username}>
+                <td>
+                  <Link to={`users/${user.id}`}>{user.username}</Link>
+                </td>
+                <td>{user.blogs.length}</td>
+              </tr>
+            ))}
       </tbody>
     </table>
   </div>
