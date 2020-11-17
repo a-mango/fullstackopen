42b73bf8bb1463d8a74ca90e44c833a70374ff0c
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-17 17:24:12 +0100

    Modify Router
    Abstract parametrized routing away from main router

--- part7/bloglist-fullstack/client/components/Router.js
+++ part7/bloglist-fullstack/client/components/Router.js
@@ -4,9 +4,13 @@ import { Route, Switch } from 'react-router-dom'
 import BlogView from 'Components/BlogView'
 import UsersView from 'Components/UsersView'
 
-export default () => (
-  <Switch>
-    <Route exact path="/" component={BlogView} />
-    <Route path="/users" component={UsersView} />
-  </Switch>
-)
+const Router = () => {
+  return (
+    <Switch>
+      <Route path="/users" component={UsersView} />
+      <Route path="/" component={BlogView} />
+    </Switch>
+  )
+}
+
+export default Router
