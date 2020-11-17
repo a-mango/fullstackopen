5f386d7b3aa2fa031ffcb063a8fcdd3c1f4f4d18
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-17 14:30:01 +0100

    Add useField hook

---
+++ part7/bloglist-fullstack/client/hooks/use-field.js
@@ -0,0 +1,20 @@
+const useField = type => {
+  const [value, setValue] = useState('')
+
+  const onChange = event => {
+    setValue(event.target.value)
+  }
+
+  const reset = () => {
+    setValue('')
+  }
+
+  return {
+    type,
+    value,
+    onChange,
+    reset,
+  }
+}
+
+export default useField
