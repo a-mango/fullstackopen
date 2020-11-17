5bd719db7c0212534569fec76eb70561d820d570
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-17 14:02:43 +0100

    Refactor app to use redux

--- part7/bloglist-fullstack/client/App.js
+++ part7/bloglist-fullstack/client/App.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { initializeBlogs } from 'Utilities/reducers/blogReducer'
-import { setUser } from 'Utilities/reducers/userReducer'
-
-import blogService from 'Utilities/services/blogs'
-import loginService from 'Utilities/services/login'
+import { setUser, clearUser } from 'Utilities/reducers/userReducer'
+import { setNotification } from 'Utilities/reducers/notificationReducer'
 
 import Nav from 'Components/Nav'
 import Footer from 'Components/Footer'
@@ -21,120 +19,41 @@ const App = () => {
   useEffect(() => {
     try {
       dispatch(initializeBlogs())
-      // Raise notification
     } catch (exception) {
-      // Raise notification
-    }
-  }, [dispatch])
-
-  useEffect(() => {
-    try {
-      const loggedUserJson = window.localStorage.getItem('loggedBlogAppUser')
-      if (loggedUserJson) {
-        const user = JSON.parse(loggedUserJson)
-        dispatch(setUser(user))
-        // Raise notification
-      }
-    } catch (exception) {
-      // Raise notification
-    }
-  }, [dispatch])
-
-  const [notification, setNotification] = useState(null)
-  const blogFormRef = useRef()
-
-  /**
-   * Handles the display of a notification message
-   */
-  const handleNotification = (type, message) => {
-    setNotification({
-      type,
-      message,
-    })
-    setTimeout(() => setNotification(null), 6000)
-  }
-
-  /**
-   * Handle login of the user by making a call to the
-   * login api and saving the returned user to state
-   * and localStorage
-   */
-  const handleLogin = async userObject => {
-    try {
-      const user = await loginService.login(userObject)
-
-      // Save user to local storage
-      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
-
-      // Set the user's token in blogService
-      blogService.setToken(user.token)
-
-      // Modify app state
-      setUser(user)
-
-      // Notify user
-      handleNotification('Success', `Authenticated as ${user.username}`)
-    } catch (exception) {
-      handleNotification(
-        'Error',
-        'Authentication failed. Verify the username and password and try again'
+      dispatch(
+        setNotification({
+          type: 'error',
+          message: 'An error has occured while fetching the data',
+        })
       )
     }
-  }
+  }, [dispatch])
+
+  const blogFormRef = useRef()
 
   /**
    * Handle logout of the user by clearing both
    * state and localStorage
    */
   const handleLogout = () => {
-    // Clear localStorage of user item
-    window.localStorage.removeItem('loggedBlogAppUser')
-    // Reset state
-    setUser(null)
+    dispatch(clearUser())
 
-    handleNotification('Success', 'User logged out')
-  }
-
-  /**
-   * Handle addition of a blog
-   */
-  const addBlog = async blogObject => {
-    try {
-      // Post the data to the server
-      const returnedBlog = await blogService.create(blogObject)
-
-      // Update returnedBlog with user.username instead of id
-      const addedBlog = {
-        ...returnedBlog,
-        user: {
-          username: user.username,
-        },
-      }
-
-      // Update state
-      setBlogs(blogs.concat(addedBlog))
-
-      // Toggle blog form
-      blogFormRef.current.toggleVisibility()
-
-      // Notify the user
-      handleNotification('Success', 'A new blog was added to the database')
-    } catch (exception) {
-      handleNotification(
-        'Error',
-        'There was a problem while adding a blog to the database'
-      )
-    }
+    dispatch(
+      setNotification({
+        type: 'info',
+        message: 'Successfully logged out',
+      })
+    )
   }
 
   /**
    * Form components
    */
-  const loginForm = () => <LoginForm loginUser={handleLogin} />
+  const loginForm = () => <LoginForm />
 
   const blogForm = () => (
     <Togglable buttonLabel="Add blog" ref={blogFormRef}>
-      <BlogForm createBlog={addBlog} />
+      <BlogForm toggleRef={blogFormRef} />
     </Togglable>
   )
 
@@ -150,7 +69,7 @@ const App = () => {
   return (
     <div>
       <Nav />
-      <Notification notification={notification} />
+      <Notification />
       {user === null ? (
         loginForm()
       ) : (
