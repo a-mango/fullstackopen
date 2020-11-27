bada70ab83b6d5691e2c78b178bb4424eb1e424b
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-27 23:37:33 +0100

    lint: remove unused import

--- part7/bloglist-fullstack/server/tests/blog_api.test.js
+++ part7/bloglist-fullstack/server/tests/blog_api.test.js
@@ -3,7 +3,6 @@ const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../index')
 const api = supertest(app)
-const bcrypt = require('bcrypt')
 const Blog = require('@models/blogModel')
 const User = require('@models/userModel')
 
