9a28e0ce36b34bf3eef5d3306541e1c2e0f4f98f
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-28 16:41:35 +0100

    feat: implement Authors view

--- part8/client/src/components/Authors.js
+++ part8/client/src/components/Authors.js
@@ -1,11 +1,27 @@
-  
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../utils/queries'
 
-const Authors = (props) => {
+const Authors = props => {
+  const [authors, setAuthors] = useState([])
+
+  const result = useQuery(ALL_AUTHORS, {
+    pollInterval: 2000,
+  })
+
+  useEffect(() => {
+    if (result.data) {
+      setAuthors(result.data.allAuthors)
+    }
+  }, [result])
+
   if (!props.show) {
     return null
   }
-  const authors = []
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -14,23 +30,18 @@ const Authors = (props) => {
         <tbody>
           <tr>
             <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
+            <th>born</th>
+            <th>books</th>
           </tr>
-          {authors.map(a =>
+          {authors.map(a => (
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
               <td>{a.bookCount}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
-
     </div>
   )
 }
