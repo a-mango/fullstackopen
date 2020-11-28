2e9506a4f55b7948a0b5547970f1ce19bf14c788
Aubry Mangold <aubry.mangold@outlook.com>
2020-11-28 01:22:36 +0100

    feat: implement editAuthor mutation

--- part8/index.js
+++ part8/index.js
@@ -114,6 +114,7 @@ const typeDefs = gql`
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `
 
@@ -157,10 +158,20 @@ const resolvers = {
         }
         authors = authors.concat(author)
       }
-      
+
       const book = { ...args, id: uuid() }
       books = books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => (a.name === args.name ? updatedAuthor : a))
+      return updatedAuthor
     },
   },
 }
