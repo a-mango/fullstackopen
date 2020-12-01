8d5fcf98d44a0bc505325a4fa2ced8725c219753
Aubry Mangold <aubry.mangold@outlook.com>
2020-12-01 18:51:50 +0100

    fix: modify ALL_BOOKS query and data rendering

--- part8/client/src/components/Books.js
+++ part8/client/src/components/Books.js
@@ -30,11 +30,11 @@ const Books = props => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map(a => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
+          {books.map(book => (
+            <tr key={book.title}>
+              <td>{book.title}</td>
+              <td>{book.author.name}</td>
+              <td>{book.published}</td>
             </tr>
           ))}
         </tbody>
--- part8/client/src/utils/queries.js
+++ part8/client/src/utils/queries.js
@@ -15,7 +15,9 @@ export const ALL_BOOKS = gql`
   query {
     allBooks {
       title
-      author
+      author {
+        name
+      }
       published
       id
     }
