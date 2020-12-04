94bb6917a7b9fdd04aa6f4838d065689109de62b
Aubry Mangold <aubry.mangold@outlook.com>
2020-12-04 19:55:30 +0100

    fix: modify directory structure

---
+++ part9/first-steps/bmiCalculator.ts
@@ -0,0 +1,12 @@
+/**
+ * BMI Calculator
+ *
+ * Write a function calculateBmi that counts BMI based on given
+ * height (in centimeters) and weight (in kilograms) and
+ * then returns a message that suits the results.
+ */
+
+interface BmiValues {
+  height: number;
+  weight: number;
+}
