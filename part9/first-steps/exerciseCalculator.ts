87e13bf916f9a0d4628ad60b7d50ad609c7d3d3c
Aubry Mangold <aubry.mangold@outlook.com>
2020-12-30 11:10:39 +0100

    feat: implement exercises calculator

---
+++ part9/first-steps/exerciseCalculator.ts
@@ -0,0 +1,71 @@
+/**
+ * Exercise Calculator
+ *
+ * Write a function calculateExercises that calculates the
+ * average time of daily exercise hours and compares it to
+ * the target amount of daily hours and returns an object
+ * that includes the following values:
+ * - the number of days
+ * - the number of training days
+ * - the original target value
+ * - the calculated average time
+ * - boolean value describing if the target was reached
+ * - a rating between the numbers 1-3 that tells how well the hours are met. You can decide on the metric on your own.
+ * - a text value explaining the rating
+ */
+
+/**
+ * The result of the exercise session
+ */
+interface ExerciseResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (
+  dailyExerciseHours: Array<number>,
+  targetAmount: number
+): ExerciseResult => {
+  const periodLength: number = dailyExerciseHours.length;
+  const trainingDays: number = dailyExerciseHours.filter((i) => i !== 0).length;
+  const average: number =
+    dailyExerciseHours.reduce((a, b) => a + b) / periodLength;
+  const success: boolean = average >= targetAmount;
+
+  let rating: number;
+  let ratingDescription: string;
+  if (average <= targetAmount / 2) {
+    rating = 1;
+    ratingDescription = 'You need to put more effort in this!';
+  } else if (average > targetAmount / 2 && average < targetAmount) {
+    rating = 2;
+    ratingDescription = 'A little more effort next time. Keep it up!';
+  } else {
+    rating = 3;
+    ratingDescription =
+      'Well done, you reached you target amout of daily exercise hours!';
+  }
+
+  const result: ExerciseResult = {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target: targetAmount,
+    average,
+  };
+
+  return result;
+};
+
+try {
+  console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+} catch (error) {
+  console.error(`Oops, an error occured. Message:\n${error.message}`);
+}
