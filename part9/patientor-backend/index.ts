ae233d6c26b2f4bf0093275bdb32d4be3a47cf4e
Aubry Mangold <aubry.mangold@outlook.com>
2021-02-16 12:38:08 +0100

    feat(9.8): initial configuration

---
+++ part9/patientor-backend/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+  res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
